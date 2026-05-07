2c3c16fd352b5affe73698d098f1369500a04425
mykeld74 <mike@msdwebsolutions.com>
2026-05-07 07:43:06 -0600

    trying to fix deployed site

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -4,13 +4,17 @@ import { svelteKitHandler } from 'better-auth/svelte-kit';
 
 /** @type {import('@sveltejs/kit').Handle} */
 export async function handle({ event, resolve }) {
-	const session = await auth.api.getSession({
-		headers: event.request.headers
-	});
+	try {
+		const session = await auth.api.getSession({
+			headers: event.request.headers
+		});
 
-	if (session) {
-		event.locals.session = session.session;
-		event.locals.user = session.user;
+		if (session) {
+			event.locals.session = session.session;
+			event.locals.user = session.user;
+		}
+	} catch (error) {
+		console.error('Unable to load auth session.', error);
 	}
 
 	return svelteKitHandler({ event, resolve, auth, building });
